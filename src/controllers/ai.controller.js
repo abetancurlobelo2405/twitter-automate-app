e75b0951c7f8e6ad6c5978b6950a95a5671becdf
xd <andres_242001@hotmail.com>
2023-01-24 16:19:40 -0500

    controllear

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -1,10 +1,9 @@
 import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 export const generatorAI = async (req, res) => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
   const text = req.body.text;
   const openai = new OpenAIApi(configuration);
   const response = await openai.createCompletion({
