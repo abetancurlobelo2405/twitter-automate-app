22428e4eebfad9eb28497f56c623ace829838854
xd <andres_242001@hotmail.com>
2023-01-24 16:13:16 -0500

    controllear ai

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -10,8 +10,8 @@ export const generatorAI = async (req, res) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: text,
-    max_tokens: 3500,
-    temperature: 0,
+    max_tokens: 3000,
+    temperature: 0.9,
   });
   console.log(response);
   res.status(200).json(response.data.choices[0].text);
